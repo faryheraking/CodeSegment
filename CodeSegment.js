988821569b2c1584e7b46796eca07fb802ad230f
faryheraking <faryheraking@sina.com>
2018-03-13 10:54:01 +0800

    test.git ssh

--- CodeSegment.js
+++ CodeSegment.js
@@ -1,9 +1,15 @@
 /* 限制字符 */
 function limitChar(str, n, errMsg) {
-    var length = str.length;
-    var charLength = str.match(/[a-zA-Z]+/g).join("").length;
+    var length = str.length, arr = str.match(/[a-zA-Z]+/g);
+    var charLength = 0;
+    if (arr && arr !== null && typeof arr == "object") {
+        charLength = arr.join("").length;
+    }
     var wordLength = length - charLength;
     if (wordLength * 2 + charLength > n) {
         alert(errMsg);
+        return false;
+    } else {
+        return true;
     }
 }
