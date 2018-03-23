4a671d6054b5620587bb13921f71a80c01fbf1bb
faryheraking <faryheraking@sina.com>
2018-03-23 09:38:40 +0800

    1111

--- CodeSegment.js
+++ CodeSegment.js
@@ -13,3 +13,26 @@ function limitChar(str, n, errMsg) {
         return true;
     }
 }
+
+var timeFormat = function (seconds) {
+    var w = Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60 % 60 / 24) : Math.floor(seconds / 60 % 60 / 24);
+    var m = Math.floor(seconds / 60) < 10 ? "0" + Math.floor(seconds / 60) : Math.floor(seconds / 60);
+    var s = Math.floor(seconds - (m * 60)) < 10 ? "0" + Math.floor(seconds - (m * 60)) : Math.floor(seconds - (m * 60));
+    return w + ":" + m + ":" + s;
+};
+
+Date.prototype.Format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
